a6435a6ebb0aecf4667f19674b176856fca8db1c
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2024-03-13 15:23:57 +0530

    Frontend Added || Auth Context Needs to be written

---
+++ frontend/src/components/ChartPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, Typography, Paper } from '@mui/material';
+
+const ChartPage = ({ data }) => {
+  return (
+    <Paper elevation={3} style={{ padding: 20, margin: 20 }}>
+      <Typography variant="h4" align="center" gutterBottom>
+        Salary Chart
+      </Typography>
+      <ResponsiveContainer width="100%" height={400}>
+        <BarChart data={data}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Bar dataKey="salary" fill="#ff0000" name="Salary less than 20k" />
+          <Bar dataKey="salary" fill="#00ff00" name="Salary greater than 100k" />
+        </BarChart>
+      </ResponsiveContainer>
+    </Paper>
+  );
+};
+
+export default ChartPage;
---
+++ frontend/src/pages/chart.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import ChartPage from '../components/ChartPage';
+import axios from 'axios';
+
+const Chart = () => {
+  const [chartData, setChartData] = useState([]);
+
+  useEffect(() => {
+    const fetchChartData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8001/api/users/user-data');
+        setChartData(response.data);
+      } catch (error) {
+        console.error('Error fetching chart data:', error.message);
+      }
+    };
+
+    fetchChartData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Chart Page</h1>
+      <ChartPage data={chartData} />
+    </div>
+  );
+};
+
+export default Chart;
